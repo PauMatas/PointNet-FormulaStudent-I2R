2954d4680dcb25ee4e419852f1febabd2f290839
Pau Biosca <pau.biosca.marron@estudiantat.upc.edu>
2022-11-02 23:10:40 +0100

    Cone Position Subscription Callbacks Reorganized

--- scripts/main.py
+++ scripts/main.py
@@ -7,21 +7,38 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 
-def pointCloudSubscriber(ros_point_cloud):
+from geometry_msgs.msg import PointStamped
+
+def pointCloudSubscriber(last_pointcloud):
     print('pointcloud')
     # gen = pc2.read_points(ros_point_cloud, skip_nans=True)
     # print(list(gen))
 
-def positionSubscriber(ros_point_cloud):
+def positionSubscriber(last_odom):
     print('odometry')
+    print(last_odom)
+
+def conePosition(cone_point):
+    print('Cone position received')
+    print('Header: ', cone_point.header.stamp)
+    print('x: ', cone_point.point.x)
+    print('y: ', cone_point.point.y)
+    print('z: ', cone_point.point.z)
+    
 
 def main():
     rospy.init_node('main')
 
     print("Hello")
 
-    rospy.Subscriber('limovelo/full_pcl', PointCloud2, pointCloudSubscriber, buff_size=10000)
-    rospy.Subscriber('limovelo/state', Odometry, positionSubscriber, buff_size=10000)
+    # LiDAR Output Subscriber
+    rospy.Subscriber('limovelo/full_pcl', PointCloud2, pointCloudSubscriberClbk, buff_size=10000)
+
+    # LIMOVelo State Subscriber
+    rospy.Subscriber('limovelo/state', Odometry, positionSubscriberClbk, buff_size=10000)
+
+    # Ground Truth Cone Point
+    rospy.Subscriber('/clicked_point', PointStamped, conePositionClbk, buff_size=10000)
     
     rospy.spin()
 
